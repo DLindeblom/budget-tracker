37a1ebb350e0ce7421880404c7c79f08852a99a7
DLindeblom <drdlindeblom@gmail.com>
2022-09-12 14:42:41 -0400

    create header with buttons and create budgetcard component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Button, Stack } from "react-bootstrap"
 import Container from "react-bootstrap/Container"
 
 function App() {
-  return <Container>
+  return <Container className="my-4">
     <Stack direction="horizontal" gap="2" className="mb-4">
       <h1 className="me-auto">Budgets</h1>
       <Button variant="primary">Add Budget</Button>
