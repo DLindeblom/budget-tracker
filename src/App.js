6d40a2e040430bfb7ddb47f9da49946d4f10afc8
DLindeblom <drdlindeblom@gmail.com>
2022-09-12 14:35:18 -0400

    install bootstrap and uuid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Button, Stack } from "react-bootstrap"
+import Container from "react-bootstrap/Container"
 
 function App() {
-  return (
-    "HAPPY!"
-  );
+  return <Container>
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className="me-auto">Budgets</h1>
+      <Button variant="primary">Add Budget</Button>
+      <Button variant="outline-primary">Add Expense</Button>
+
+    </Stack>
+  </Container>
 }
 
 export default App;
