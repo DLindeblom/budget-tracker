d454a909a2475bab214423234678de424fec2ee9
DLindeblom <drdlindeblom@gmail.com>
2022-09-13 12:07:53 -0400

    create totalbudgetcard

---
+++ src/components/TotalBudgetCard.js
@@ -0,0 +1,13 @@
+import { useBudgets } from "../contexts/BudgetsContext"
+import BudgetCard from "./BudgetCard"
+
+export default function TotalBudgetCard(props) {
+  const { getBudgetExpenses } = useBudgets()
+  const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce(
+    (total, expense) => total + expense.amount,
+    0
+  )
+  if (amount === 0) return null
+
+  return <BudgetCard amount={amount} name="Total" gray {...props} />
+}
