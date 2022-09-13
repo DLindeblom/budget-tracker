6de85da7334475fdb8b1188d79c438e4d2e37893
DLindeblom <drdlindeblom@gmail.com>
2022-09-13 13:29:17 -0400

    define budget, create delete button to delete budget

--- src/components/ViewExpensesModal.js.js
+++ src/components/ViewExpensesModal.js.js
@@ -1,33 +1,33 @@
-import {  Modal, Button } from "react-bootstrap"
-import { useBudgets } from '../contexts/BudgetsContext'
+import { Modal, Button } from "react-bootstrap";
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from '../contexts/BudgetsContext';
 
-export default function AddBudgetModal({ show, handleClose }) {
-  const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } = useBudgets()
+export default function AddBudgetModal({ budgetId, handleClose }) {
+  const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } = useBudgets();
 
+
+  const budget = UNCATEGORIZED_BUDGET_ID === budgetId ? { name: "Uncatgorized", id: UNCATEGORIZED_BUDGET_ID } : budgets.find(b => b.id === budgetId);
   return (
-    <Modal show={show} onHide={handleClose}>
-      <Form onSubmit={handleSubmit}>
-        <Modal.Header closeButton>
-          <Modal.Title>New Budget</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control ref={nameRef} type="text" required/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Maximum Spending</Form.Label>
-            <Form.Control 
-              ref={maxRef} 
-              type="number" 
-              required 
-              min={0} step={.01}/>
-          </Form.Group>
-          <div className="d-flex justify-content-end">
-            <Button varitant="primary" type="submit">Add</Button>
-          </div>
-        </Modal.Body>
-      </Form>
+    <Modal show={budgetId != null} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <Stack direction="horizontal" gap="2">
+            <div>Expenses - {budget?.name}</div>
+            {budgetId !== UNCATEGORIZED_BUDGET_ID && (
+              <Button
+                onClick={() => {
+                  deleteBudget(budget);
+                  handleClose();
+                }} 
+                variant="outline-danger"
+              >
+                Delete
+              </Button>
+            )}
+          </Stack>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      </Modal.Body>
     </Modal>
-  )
+  );
 }
