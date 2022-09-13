6df3c10ca6f75686dbee0f8589c6c997350516d7
DLindeblom <drdlindeblom@gmail.com>
2022-09-13 12:07:40 -0400

    make progress bar and max amount conditionsl

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -2,19 +2,19 @@ import { Card, ProgressBar, Stack, Button } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
 
 
-export default function BudgetCard({ 
-  name, 
-  amount, 
-  max, 
-  gray, 
-  onAddExpenseClick 
+export default function BudgetCard({
+  name,
+  amount,
+  max,
+  gray,
+  onAddExpenseClick
 }) {
-  const classNames = []
+  const classNames = [];
 
   if (amount > max) {
-    classNames.push("bg-danger", "bg-opacity-10")
+    classNames.push("bg-danger", "bg-opacity-10");
   } else if (gray) {
-    classNames.push("bg-light")
+    classNames.push("bg-light");
   }
 
   return (
@@ -24,25 +24,29 @@ export default function BudgetCard({
           <div className="me-2">{name}</div>
           <div className="d-flex align-items-baseline">
             {currencyFormatter.format(amount)}
-            <span className="text-muted fs-6 ms-1">
-              / {currencyFormatter.format(max)}
-            </span>
+            {max && (
+              <span className="text-muted fs-6 ms-1">
+                / {currencyFormatter.format(max)}
+              </span>
+            )}
           </div>
         </Card.Title>
-        <ProgressBar
-          className="rounded-pill"
-          variant={getProgressBarVariant(amount, max)}
-          min={0}
-          max={max}
-          now={amount}
-        />
+        {max && (
+          <ProgressBar
+            className="rounded-pill"
+            variant={getProgressBarVariant(amount, max)}
+            min={0}
+            max={max}
+            now={amount}
+          />
+        )}
         <Stack direction="horizontal" gap="2" className="mt-4">
-          <Button 
-            variant="outline-primary" 
-            className="ms-auto" 
+          <Button
+            variant="outline-primary"
+            className="ms-auto"
             onClick={onAddExpenseClick}
-            >
-              Add Expense
+          >
+            Add Expense
           </Button>
           <Button variant="outline-secondary">View Expenses</Button>
         </Stack>
