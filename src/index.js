ca3e011dfacc2bddcc417f3b44c748eadc4192d6
DLindeblom <drdlindeblom@gmail.com>
2022-09-23 14:38:28 -0400

    add react-router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import { BudgetsProvider } from "./contexts/BudgetsContext";
+import Auth0ProviderWithHistory from './auth/Auth0ProviderWithHistory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BudgetsProvider>
-      <App />
-    </BudgetsProvider>
+    <Router>
+      <Auth0ProviderWithHistory>
+        <BudgetsProvider>
+          <App />
+        </BudgetsProvider>
+      </Auth0ProviderWithHistory>
+    </Router>
+
   </React.StrictMode>
 );
 
